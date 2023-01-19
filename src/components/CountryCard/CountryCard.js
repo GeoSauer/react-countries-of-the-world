a35a4331766d6311f9a44298042ee978fd32cf93
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-18 20:41:49 -0700

    add last bit o styling for tonight

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -7,7 +7,7 @@ export default function CountryCard({ name, iso2, local_name }) {
       <div className="image">
         <img src={`https://flagcdn.com/160x120/${iso2.toLowerCase()}.png`} />
       </div>
-      <div>{local_name}</div>
+      <div className="name">{local_name}</div>
     </div>
   );
 }
