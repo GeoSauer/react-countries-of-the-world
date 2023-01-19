9b4de0a22dbba700392db7269a57b7afe89e9be0
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-18 20:24:12 -0700

    add footer and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/layout/Header/Header';
 import Main from './components/layout/Main/Main';
+import Footer from './components/layout/Footer/Footer';
 
 export default function App() {
   return (
     <section>
       <Header />
       <Main />
+      <Footer />
     </section>
   );
 }
